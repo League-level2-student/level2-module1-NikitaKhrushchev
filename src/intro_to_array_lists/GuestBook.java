ab7fa7c81c603b97832518a5973291ed018ea266
League Student <league@iMac-27.attlocal.net>
2020-03-05 18:27:49 -0800

    changes

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,6 +1,55 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GuestBook implements ActionListener {
+	JFrame frame;
+	JTextField n = new JTextField(20);
+	JPanel panel;
+	String f = new String();
+	String[] nameList = new String[10];
+	int currentNum = 0;
+	JButton addName = new JButton();
+	JFrame addingPanel;
+	JLabel names = new JLabel();
+	JLabel l = new JLabel();
+	public static void main(String[] args) {
+		new GuestBook().start();
+		
+	}
+	public void start() {
+		f = "";
+		frame = new JFrame();
+		panel = new JPanel();
+		panel.add(names);
+		panel.add(addName);
+		addName.setText("add name");
+		addName.addActionListener(this);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	public void nextName() {
+		addingPanel = new JFrame();
+		JPanel s = new JPanel();
+		
+		JButton done = new JButton("Done");
+		s.add(n);
+		s.add(done);
+		addingPanel.add(s);
+		done.addActionListener(this);
+		addingPanel.pack();
+		addingPanel.setVisible(true);
+		addingPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +58,26 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton s = (JButton) e.getSource();
+		if(s == addName) {
+		this.nextName();
+		}
+		else {
+			nameList[currentNum] = n.getText();
+			currentNum +=1;
+			addingPanel.dispose();
+			if(currentNum-1 > 0) {
+			f = f + ", "+nameList[currentNum-1];
+			}
+			else {
+				f = nameList[currentNum-1];
+			}
+			System.out.println(f);
+			names.setText(f);
+		}
+		
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,11 +1,24 @@
 package intro_to_array_lists;
-
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		String[] s = new String[5];
+		s[0] = "I'll have two number nines";
+		s[1] = "A number nine large";
+		s[2] = "A number six with extra dip";
+		s[3] = "Two number forty-fives one with cheese";
+		s[4] = "A number seven, and a large soda.";
 		//2. Add five Strings to your list
+		for (int i = 4; i >= 0;i--) {
+			//if (i+1 == 0) {
+			System.out.println(s[i]);
+			IntStream sd = s[i].chars();
+			System.out.println(sd);
+	//	}
+		}
 		
 		//3. Print all the Strings using a standard for-loop
 		
